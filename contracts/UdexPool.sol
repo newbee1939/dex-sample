9954d689bab96c1cb78f283766a9267300a186b5
newbee1939 <hideto3970@gmail.com>
2023-10-11 21:00:48 +0900

    udpate

--- contracts/UdexPool.sol
+++ contracts/UdexPool.sol
@@ -3,9 +3,15 @@
 pragma solidity ^0.8.19;
 
 contract UdexPool {
+    // これら3つはstate variables
     // immutable: 一度登録したら変更できない
     address immutable public factory; // PoolContractをデプロイしたFactoryコントラクトのアドレスを入れる
     // このPoolに対応するTokenペアのアドレス
     address public token0;
     address public token1;
+
+    // コントラクトをデプロイするときに呼び出される
+    constructor() {
+        //
+    }
 }
