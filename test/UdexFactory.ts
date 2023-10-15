34857778eb04dbc202a43f7a373e686442f4b59c
newbee1939 <hideto3970@gmail.com>
2023-10-16 07:53:06 +0900

    update

--- test/UdexFactory.ts
+++ test/UdexFactory.ts
@@ -1,23 +1,29 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { ContractFactory, Contract } from "ethers";
 
+// "0x" から始まり、40文字の16進数で構成される必要がある
 const TEST_ADDRESSES: [string, string] = [
-  '0x12340000000000000000000000000000000000000',
-  '0x34560000000000000000000000000000000000000',
-]
+  "0x1234000000000000000000000000000000000000",
+  "0x3456000000000000000000000000000000000000",
+];
 // hardhat networkという、イーサリアムを模したテストネットワークが使われる
 describe("UdexFactory", () => {
-  it("get no pool address before creation", async () => {
+  async function deployFactoryFixture() {
     // UdexFactoryの情報を読み込む
-    const Factory = await ethers.getContractFactory(
-      "UdexFactory"
-    );
+    const Factory = await ethers.getContractFactory("UdexFactory");
     // コントラクトをデプロイするというトランザクションの情報をイーサリアムネットワークに送る
     const factory = await Factory.deploy();
     // コントラクトの情報がイーサリアム上に書き込まれたことを確認する
     await factory.deployed();
-    
-    expect(await factory.getPool(...TEST_ADDRESSES)).to.eq(ethers.constants.AddressZero);
+    return { factory };
+  }
+
+  it("get no pool address before creation", async () => {
+    const { factory } = await deployFactoryFixture();
+    expect(await factory.getPool(...TEST_ADDRESSES)).to.eq(
+      ethers.constants.AddressZero
+    );
   });
 });
