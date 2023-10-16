c82bb522c54e58ebd76fbed178382349abc08710
newbee1939 <hideto3970@gmail.com>
2023-10-17 07:31:40 +0900

    update

--- test/UdexFactory.ts
+++ test/UdexFactory.ts
@@ -1,7 +1,9 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import UdexPool from "../artifacts/contracts/UdexPool.sol/UdexPool.json";
 import { ContractFactory, Contract } from "ethers";
+import { getCreate2Address } from "./lib/utilities";
 
 // "0x" から始まり、40文字の16進数で構成される必要がある
 const TEST_ADDRESSES: [string, string] = [
@@ -37,6 +39,34 @@ describe("UdexFactory", () => {
     // 処理中に発行されたEventはレシートに書き込まれる
     const receipt = await tx.wait();
     const event = factory.interface.parseLog(receipt.logs[0]);
-    expect(event.name).to.eq("");
+    expect(event.name).to.eq("PoolCreated");
+    const poolAddress: string = event.args[2];
+    // poolのアドレスがmappingのstorgeに書き込まれたことをテスト
+    expect(await factory.getPool(TEST_ADDRESSES[1], TEST_ADDRESSES[0])).to.eq(
+      poolAddress
+    );
+    expect(await factory.getPool(TEST_ADDRESSES[0], TEST_ADDRESSES[1])).to.eq(
+      poolAddress
+    );
+  });
+
+  it("pool created at expected address", async () => {
+    const { factory } = await loadFixture(deployFactoryFixture);
+    const bytecode: string = UdexPool.bytecode;
+    const [address0, address1] =
+      TEST_ADDRESSES[0] < TEST_ADDRESSES[1]
+        ? TEST_ADDRESSES
+        : [TEST_ADDRESSES[0], TEST_ADDRESSES[1]];
+    // デプロイアドレスを取得する
+    const create2Address = getCreate2Address(
+      factory.address,
+      [address0, address1],
+      bytecode
+    );
+
+    // to.emitでEventが出ていることをテスト
+    await expect(factory.createPool(...TEST_ADDRESSES))
+      .to.emit(factory, "PoolCreated")
+      .withArgs(address0, address1, create2Address);
   });
 });
